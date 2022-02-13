8fefd1858c93dbb2f3086f39bf932e60193dc711
stefanom13 <stefanomancuso13@gmail.com>
2022-02-13 17:29:49 +0100

    snack6 inserito

--- snack6/js/app.js
+++ snack6/js/app.js
@@ -1,2 +1,12 @@
 // Stampa il cubo dei primi N numeri, dove N è un numero 
 // indicato dall’utente.
+
+const numberUSer = parseInt(prompt('inserisci un numero'))
+
+for (let index = 1; index < numberUSer; index++) {
+    const element = [index];
+    let numberCube =[index]**3
+    
+    console.log(`il cubo dei primi ${numberUSer} numeri è ${numberCube}`)
+    
+}
