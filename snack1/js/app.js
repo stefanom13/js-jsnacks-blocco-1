e4d670dd59936779be01b921d39ce7d41f9eda08
stefanom13 <stefanomancuso13@gmail.com>
2022-02-13 11:28:05 +0100

    snack1 completo

--- snack1/js/app.js
+++ snack1/js/app.js
@@ -1,3 +1,19 @@
 
 // L’utente inserisce due numeri in successione, con due 
 // prompt. Il software stampa il maggiore
+
+// creazione variabili 
+let firstNumber = parseFloat (prompt('inserisci un numero'))
+let secondNumber = parseFloat (prompt('inserisci altro numero'))
+console.log(firstNumber,secondNumber)
+
+// creo variabile per if 
+let numberMagg = firstNumber
+
+// inizializzo if per vedere quale numero sia maggiore 
+if (firstNumber < secondNumber) {
+    numberMagg = secondNumber
+}
+
+// stampo numero maggiore 
+   console.log(`numero maggiore = ${numberMagg}`)    
