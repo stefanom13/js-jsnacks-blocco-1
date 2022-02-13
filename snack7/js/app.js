10772c77e485d0c12b74cf755fb87c0c54a6c907
stefanom13 <stefanomancuso13@gmail.com>
2022-02-13 17:36:31 +0100

    inserimento snack7

--- snack7/js/app.js
+++ snack7/js/app.js
@@ -1,1 +1,8 @@
 // Stampa le potenze di 2 fino a 1000 .
+const numero = 2
+
+for (let index = 0; index < 1000; index++) {
+    const element = [index];
+    potenzaDue = numero**element
+    console.log(potenzaDue)
+}
