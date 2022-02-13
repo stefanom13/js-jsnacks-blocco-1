2321c6096bff0f25d76c4da2803369fde4470244
stefanom13 <stefanomancuso13@gmail.com>
2022-02-13 17:13:04 +0100

    jsnack-4 completato

--- snack4/js/app.js
+++ snack4/js/app.js
@@ -1,3 +1,36 @@
 // In un array sono contenuti i nomi degli invitati alla festa del 
 // grande Gatsby, chiedi all’utente il suo nome e comunicagli se 
 // può partecipare o no alla festa.
+
+const listaName = [
+    'luca',
+    'michele',
+    'enzo',
+    'bernando',
+    'massimo',
+    'giulio',
+    'nina',
+    'rosario',
+    'guidobaldo',
+  ]
+
+  const nameinList = prompt('ciao, inserisci il tuo nome');
+  console.log(nameinList);
+
+  let control = false;
+
+  for (let index = 0; index < listaName.length; index++) {
+      const element = listaName[index];
+      console.log(element)
+
+      if (element === nameinList) {
+          control = true;
+          
+      }
+      
+  }
+  if (control === true) {
+      alert('prego, puoi entrare')
+  } else {
+      alert('mi dispiace , accesso negato ')
+  }
