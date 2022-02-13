076438baaafe6c94d0c9b725f4d9f20d562a1247
stefanom13 <stefanomancuso13@gmail.com>
2022-02-13 11:59:13 +0100

    snack 2 completato

--- snack2/js/app.js
+++ snack2/js/app.js
@@ -1,3 +1,32 @@
 // L’utente inserisce due parole in successione, con due 
 // prompt. Il software stampa prima la parola più corta, poi la 
 // parola più lunga.
+
+// creare array 
+
+let parole = [];
+
+while ( parole.length !== 2 ) {
+    const parola = prompt("inserisci una parola");
+
+    if (parola.length > 0) {
+        parole.push(parola)
+    }
+    
+}
+
+const [parolaA, parolaB] = parole;
+
+let maggiore = `${parolaA} ${parolaB}`;
+console.log (maggiore)
+
+if (parolaB.length > parolaA.length) {
+    maggiore = `La parola più lunga è ${parolaB}, la più corta è ${parolaA}`;
+} else if (parolaA.length == parolaB.length) {
+    maggiore = "le parole sono lunghe uguali"
+};
+// stampare prima parola più corta e poi più lunga
+console.log(maggiore);
+   
+     
+ 
