bf14ed9b1b85cce9a697d2dde2a1b044925b148e
stefanom13 <stefanomancuso13@gmail.com>
2022-02-13 17:56:56 +0100

    esercizio completato

--- snack9/js/app.js
+++ snack9/js/app.js
@@ -1,1 +1,21 @@
 // Calcola la somma e la media dei primi 10 numeri. 
+
+const listNumber = []
+
+for (let index = 1; index <= 10; index++) {
+    const numbers = index;
+    console.log(numbers)
+    listNumber.push(numbers)
+    
+}
+
+let totale = 0
+for (let index = 0; index < listNumber.length; index++) {
+    totale += listNumber[index]
+    
+}
+console.log(totale)
+
+const medium = totale/10
+
+console.log(medium)
