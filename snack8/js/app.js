88dde46c75783218660afb45c0415ad8d7df5618
stefanom13 <stefanomancuso13@gmail.com>
2022-02-13 17:50:15 +0100

    snacks8 inserito

--- snack8/js/app.js
+++ snack8/js/app.js
@@ -1,2 +1,26 @@
-// Chiedi un numero di 4 cifre all’utente e calcola la somma 
-// di tutte le cifre che compongono il numero.
+// Chiedi un numero di 4 cifre all’utente e 
+// calcola la somma  di tutte le cifre che compongono il numero.
+
+const numberUser = prompt('inserisci un numero di 4 cifre');
+console.log(numberUser);
+
+const singleNumber = numberUser.split('');
+console.log(singleNumber);
+
+const number = []
+for (let index = 0; index < singleNumber.length; index++) {
+    number.push(parseInt(singleNumber[index]));
+    
+}
+console.log(number)
+
+let sommaNumeri = 0
+
+for (let index = 0; index < number.length; index++) {
+    sommaNumeri += number[index]
+    
+}
+
+
+console.log(`La somma del numero da te inserito è = ${sommaNumeri}`)  
+
