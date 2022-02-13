db3ac4324765fdb8acec405fdbec6345fff6c465
stefanom13 <stefanomancuso13@gmail.com>
2022-02-13 17:21:35 +0100

    snacks5 completato

--- snack5/js/app.js
+++ snack5/js/app.js
@@ -1,2 +1,20 @@
-// Crea un array vuoto. Chiedi per 6 volte all’utente di inserire 
-// un numero, se è dispari inseriscilo nell’array.
+// Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero
+// se è dispari inseriscilo nell’array.
+
+const numeriPari =[]
+const numeriDispari =[]
+
+for (let index = 0; index < 6; index++) {
+    numberUser = parseInt(prompt('inserisci un numero'))
+    console.log(numberUser)
+    if (numberUser %2 == 0) {
+        numeriPari.push(numberUser)
+        
+    } else {
+        numeriDispari.push(numberUser)
+        
+    }
+}
+
+console.log(numeriPari)
+console.log(numeriDispari)
